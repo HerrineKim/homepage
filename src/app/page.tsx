3eb64e9a12420ea70609e6abaf171b38efbd9cdc
hyerin <herrine.kim@gmail.com>
2023-02-03 10:46:30 +0900

    🎨feat : <meta> 태그 최초 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,25 +8,14 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
+        <p>안냐세요 혜린이에요</p>
         <div>
           <a
             href='https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
             target='_blank'
             rel='noopener noreferrer'
           >
-            By{' '}
-            <Image
-              src='/vercel.svg'
-              alt='Vercel Logo'
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+            By 혜린
           </a>
         </div>
       </div>
