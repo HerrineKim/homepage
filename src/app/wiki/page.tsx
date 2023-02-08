a1a23aceda5949f9da321796bb91933b16aa6a65
herrinekim <herrine.kim@gmail.com>
2023-02-09 01:02:40 +0900

    🎨feat : MD 파일 본문 가져오기 구현

--- src/app/wiki/page.tsx
+++ src/app/wiki/page.tsx
@@ -4,7 +4,8 @@ import Link from 'next/link'
 
 const getPostMetadata = () => {
   // 왜 Next.js에서는 escape character를 두 번 써야 하나?
-  const folder = 'src\\app\\wiki\\posts'
+  const folder = 'src\\app\\posts'
+  // TODO 비동기 방식으로 변경
   const files = fs.readdirSync(folder)
   const markdownPosts = files.filter((file) => file.endsWith('.md'))
   // slugs: 파일명에서 확장자를 제외한 배열. 왜 slug? 옛날에 신문에서 중요한 단어로만 제목 만든 것을 지칭했습니다.
