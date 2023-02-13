6e73c3c9e89e6a78e401aa8f76ee5afe765530cb
herrinekim <herrine.kim@gmail.com>
2023-02-14 01:08:27 +0900

    🎨feat : metadata 출력 및 SSG 적용중

--- src/app/wiki/page.tsx
+++ src/app/wiki/page.tsx
@@ -1,30 +1,17 @@
-import fs from 'fs'
-// 파일 관련 입출력 등 여러 처리를 해주는 node.js 내장 모듈입니다.
 import Link from 'next/link'
-
-const getPostMetadata = () => {
-  // 왜 Next.js에서는 escape character를 두 번 써야 하나?
-  const folder = 'src\\app\\posts'
-  // TODO 비동기 방식으로 변경
-  const files = fs.readdirSync(folder)
-  const markdownPosts = files.filter((file) => file.endsWith('.md'))
-  // slugs: 파일명에서 확장자를 제외한 배열. 왜 slug? 옛날에 신문에서 중요한 단어로만 제목 만든 것을 지칭했습니다.
-  const slugs = markdownPosts.map((file) => file.replace('.md', ''))
-  return slugs
-}
+import getPostMetadata from '../components/getPostMetadata'
 
 export default function WikiPage() {
   const postMetadata = getPostMetadata()
-  return (
+  const postPreviews = postMetadata.map((post) => (
     <>
-      <h1>Wiki</h1>
-      <ul>
-        {postMetadata.map((slug) => (
-          <li key={slug}>
-            <Link href={`/wiki/${slug}`}>{slug}</Link>
-          </li>
-        ))}
-      </ul>
+      <Link href={`/wiki/${post.slug}`}>
+        <h2>{post.title}</h2>
+      </Link>
+      <p>{post.date}</p>
+      <p>{post.tags}</p>
     </>
-  )
+  ))
+
+  return <div>{postPreviews}</div>
 }
