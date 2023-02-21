28693d32dfe7f2b2d34a0350fa74de230ec30722
hyerin <herrine.kim@gmail.com>
2023-02-21 11:21:46 +0900

    🎨feat : 책 검색 결과 출력

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,18 +1,23 @@
-import { getBookDetail } from '../lib/books/getBookDetail'
+import { getBookDetail } from '@/app/lib/books/getBookDetail'
 
-export default function BooksPage() {
-  const bookData = getBookDetail('리버보이')
-
-  console.log(
-    bookData.then((data) => {
-      console.log(data)
-    }),
-  )
+export default async function BooksPage() {
+  const bookData = await getBookDetail('리버보이')
+  // console.log('🚀 ~ file: page.tsx:5 ~ BooksPage ~ bookData:', bookData)
+  console.log(bookData.items)
 
   return (
     <>
-      <h1>Experiment</h1>
-      <div></div>
+      <h1>books</h1>
+      <ul>
+        {bookData.items.map((item: any) => (
+          <li key={item.isbn}>
+            <h2>{item.title}</h2>
+            <p>{item.author}</p>
+            <p>{item.description}</p>
+            <img src={item.image} />
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
