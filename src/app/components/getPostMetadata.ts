9d93e688789a22bc3d9d5b5375e85203816f0f72
hyerin <herrine.kim@gmail.com>
2023-03-27 14:48:10 +0900

    🐛fix : path 모듈 적용

--- src/app/components/getPostMetadata.ts
+++ src/app/components/getPostMetadata.ts
@@ -2,16 +2,17 @@ import fs from 'fs'
 // 파일 관련 입출력 등 여러 처리를 해주는 node.js 내장 모듈입니다.
 import matter from 'gray-matter'
 import { PostMetadata } from '@/app/components/PostMetadata'
+import path from 'path'
 
 const getPostMetadata = (): PostMetadata[] => {
-  const folder = 'src\\app\\posts'
+  const folder = path.join('src', 'app', 'posts')
   // TODO 비동기 방식으로 변경
   const files = fs.readdirSync(folder)
   const markdownPosts = files.filter((file) => file.endsWith('.md'))
   // slugs: 파일명에서 확장자를 제외한 배열. 왜 slug? 옛날에 신문에서 중요한 단어로만 제목 만든 것을 지칭했습니다.
 
   const posts = markdownPosts.map((file) => {
-    const fileContents = fs.readFileSync(`${folder}\\${file}`, 'utf8')
+    const fileContents = fs.readFileSync(`${path.join(folder, file)}`, 'utf8')
     const matterResult = matter(fileContents)
     return {
       title: matterResult.data.title,
